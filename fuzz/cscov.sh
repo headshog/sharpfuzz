c6f350765c79826455b037f1ced560d29d0b0198
headshog <craaaaaachind@gmail.com>
2024-02-05 23:19:07 +0300

    cscov x2

--- fuzz/cscov.sh
+++ fuzz/cscov.sh
@@ -2,14 +2,9 @@
 
 #echo -n "{}" > coverage.json
 
-dotnet restore
-dotnet build
-
-./minicover instrument --sources Program.cs --sources ../Jil/
-
-num_files=$(ls corpus | wc -l)
-cur_file=1
-
+#num_files=$(ls corpus | wc -l)
+#cur_file=1
+#
 #for i in corpus/*
 #do
 #    echo "Collecting coverage of file: $cur_file/$num_files"
@@ -22,9 +17,20 @@ cur_file=1
 #rm -rf coverage.lcov && mv coverage.info coverage.lcov
 #genhtml -o jil-html coverage.lcov
 
+
+rm -rf coverage.json coverage-hits ../coverage.json ../coverage-hits
+
+dotnet restore
+dotnet build
+
+./minicover instrument --sources Program.cs --sources ../Jil/
+#./minicover instrument --workdir /home/headshog/sharpfuzz/ --sources fuzz/Program.cs --sources Jil/
+
+./minicover reset
+
 dotnet run --no-build < corpus/1
 
 ./minicover uninstrument
 
 echo "Generating coverage"
-./minicover report --threshold 90
+./minicover report --threshold 90 --no-fail
